bc90cc98a02c6c84baefba066f86445c92a0ae2d
SimonKeane11 <113618007+SimonKeane11@users.noreply.github.com>
2024-02-27 22:32:45 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,1 +1,12 @@
+import datetime as dt
+import requests
 
+BASE_URL = "http://api.openweathermap.org?"
+API_Key = open('api_key', 'r'). read()
+CITY = "Greenville"
+
+url = BASE_URL + "appid=" = API_KEY + "&q=" = CITY
+
+response = requests.get(url).json()
+
+print(response)
